02fb393341946cf581a608c6a5bb85b8eef8c3d0
51494794 <51494794@qq.com>
2017-10-24 16:09:05 +0800

    view2 sample

--- app/page/view2/view2.js
+++ app/page/view2/view2.js
@@ -1,3 +1,26 @@
-app.controller('View2Ctrl', [function() {
+app.controller('View2Ctrl', function($scope, $q, $http) {
 
-}]);
+    $scope.init = function() {
+        $scope.getData().then(function(data){
+            console.log(JSON.stringify(data));
+        }, function (err) {
+            console.log(JSON.stringify(err));
+        });
+    };
+
+    $scope.getData = function() {
+        var defer = $q.defer();
+        $http({
+            method: 'GET',
+            url: '/page/view2/view2.json'
+        }).then(function(data){
+            defer.resolve(data);
+        }, function(err){
+            defer.reject(err);
+        });
+        return defer.promise;
+    };
+
+    $scope.init();
+
+});
